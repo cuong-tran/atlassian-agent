6aa77a7b1769e63685e565538c2f8368a1181d08
InoriAsuka <33611921+InoriHimea@users.noreply.github.com>
2024-07-11 13:14:11 +0800

    Update KeyTransformer.java
    change get lib path by classpath

--- src/main/java/io/zhile/crack/atlassian/agent/KeyTransformer.java
+++ src/main/java/io/zhile/crack/atlassian/agent/KeyTransformer.java
@@ -36,22 +36,32 @@ public class KeyTransformer implements ClassFileTransformer {
     }
 
     private byte[] handleKeySpec() throws IllegalClassFormatException {
+        String parseMethod = "Base64.getDecoder().decode";
+
         try {
             ClassPool cp = ClassPool.getDefault();
-            cp.importPackage("java.util.Arrays");
-            cp.importPackage("javax.xml.bind.DatatypeConverter");
+            cp.importPackage("java.util");
+            cp.importPackage("javax.xml.bind");
 
             int mod = Modifier.PRIVATE | Modifier.STATIC | Modifier.FINAL;
             CtClass cc = cp.get(CN_KEY_SPEC.replace('/', '.'));
             CtClass cb = cp.get("byte[]");
-            CtField cfOld = new CtField(cb, "__h_ok", cc);
+            CtField cfOld1 = new CtField(cb, "__h_ok1", cc);
+            CtField cfOld2 = new CtField(cb, "__h_ok2", cc);
             CtField cfNew = new CtField(cb, "__h_nk", cc);
-            cfOld.setModifiers(mod);
+            cfOld1.setModifiers(mod);
+            cfOld2.setModifiers(mod);
             cfNew.setModifiers(mod);
-            cc.addField(cfOld, "DatatypeConverter.parseBase64Binary(\"MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAIvfweZvmGo5otwawI3no7Udanxal3hX2haw962KL/nHQrnC4FG2PvUFf34OecSK1KtHDPQoSQ+DHrfdf6vKUJphw0Kn3gXm4LS8VK/LrY7on/wh2iUobS2XlhuIqEc5mLAUu9Hd+1qxsQkQ50d0lzKrnDqPsM0WA9htkdJJw2nS\");");
-            cc.addField(cfNew, "DatatypeConverter.parseBase64Binary(\"MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAO0DidNibJHhtgxAnM9NszURYU25CVLAlwFdOWhiUkjrjOY459ObRZDVd35hQmN/cCLkDox7y2InJE6PDWfbx9BsgPmPvH75yKgPs3B8pClQVkgIpJp08R59hoZabYuvm7mxCyDGTl2lbrOi0a3j4vM5OoCWKQjIEZ28OpjTyCr3\");");
+            cc.addField(cfOld1, parseMethod + "(\"MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAIvfweZvmGo5otwawI3no7Udanxal3hX2haw962KL/nHQrnC4FG2PvUFf34OecSK1KtHDPQoSQ+DHrfdf6vKUJphw0Kn3gXm4LS8VK/LrY7on/wh2iUobS2XlhuIqEc5mLAUu9Hd+1qxsQkQ50d0lzKrnDqPsM0WA9htkdJJw2nS\");");
+            cc.addField(cfOld2, parseMethod + "(\"MIIBtzCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYQAAoGALZHuJwQzgGnYm/X9BkMcewYQnWjMIGWHd9Yom5Qw7cVIdiZkqpiSzSKurO/WAHHLN31obg7NgGkitWUysECRE3zuJVbKGhx9xjVMnP6z5SwI89vB7Gn7UWxoCvT0JZgcMyQobXeVBtM9J3EgzkdDx/+Dck7uz/l1y+HDNdRzW00=\")");
+            cc.addField(cfNew, parseMethod + "(\"MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAO0DidNibJHhtgxAnM9NszURYU25CVLAlwFdOWhiUkjrjOY459ObRZDVd35hQmN/cCLkDox7y2InJE6PDWfbx9BsgPmPvH75yKgPs3B8pClQVkgIpJp08R59hoZabYuvm7mxCyDGTl2lbrOi0a3j4vM5OoCWKQjIEZ28OpjTyCr3\");");
             CtConstructor cm = cc.getConstructor("([B)V");
-            cm.insertBeforeBody("if(Arrays.equals($1,__h_ok)){$1=__h_nk;System.out.println(\"============================== agent working ==============================\");}");
+            cm.insertBefore("if(Arrays.equals($1,__h_ok1) || Arrays.equals($1,__h_ok2)){" +
+                        "$1=__h_nk;" +
+                        "System.out.println(\"============================== agent working ==============================\");" +
+                    "}");
+
+            cc.writeFile(new File("").getAbsolutePath());
 
             return cc.toBytecode();
         } catch (Exception e) {
@@ -67,11 +77,22 @@ public class KeyTransformer implements ClassFileTransformer {
      * @throws IllegalClassFormatException 当某些地方出问题了就会抛出这个异常
      */
     private byte[] handleLicenseDecoder() throws IllegalClassFormatException {
-        try {
+       try {
             // 我不知道怎么从 com.atlassian.bitbucket.internal.launcher.BitbucketServerLauncher 读取这个路径，所以我直接 HARD CODE
             // Forgive me pls...
-            File libs = new File("/opt/atlassian/bitbucket/7.21.0/app/WEB-INF/lib");
+            //File libs = new File("/opt/atlassian/bitbucket/7.21.0/app/WEB-INF/lib");
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            URL path = classLoader.getResource("");
+            System.out.println("==========================pppppppppppppppppp==============================");
+            System.out.println(path);
+            System.out.println("==========================pppppppppppppppppp==============================");
             ClassPool cp = ClassPool.getDefault();
+
+            URI uri = Objects.requireNonNull(path).toURI();
+            File libs = new File(Path.of(uri).toFile().getParent(), "lib");
+            System.out.println("==========================qqqqqqqqqqqqqqqqqq==============================");
+            System.out.println(libs.getAbsolutePath());
+            System.out.println("==========================qqqqqqqqqqqqqqqqqq==============================");
 
             Arrays.stream(Objects.requireNonNull(libs.listFiles())).map(File::getAbsolutePath).forEach((it) -> {
                 try {
@@ -81,36 +102,20 @@ public class KeyTransformer implements ClassFileTransformer {
                 }
             });
 
-            cp.importPackage("com.atlassian.extras.common.LicenseException");
-            cp.importPackage("com.atlassian.extras.common.org.springframework.util.DefaultPropertiesPersister");
-            cp.importPackage("com.atlassian.extras.decoder.api.AbstractLicenseDecoder");
-            cp.importPackage("com.atlassian.extras.decoder.api.LicenseVerificationException");
-            cp.importPackage("com.atlassian.extras.keymanager.KeyManager");
-            cp.importPackage("com.atlassian.extras.keymanager.SortedProperties");
-            cp.importPackage("java.io.ByteArrayInputStream");
-            cp.importPackage("java.io.ByteArrayOutputStream");
-            cp.importPackage("java.io.DataInputStream");
-            cp.importPackage("java.io.DataOutputStream");
-            cp.importPackage("java.io.IOException");
-            cp.importPackage("java.io.InputStream");
-            cp.importPackage("java.io.InputStreamReader");
-            cp.importPackage("java.io.OutputStream");
-            cp.importPackage("java.io.Reader");
-            cp.importPackage("java.io.StringWriter");
-            cp.importPackage("java.io.Writer");
-            cp.importPackage("java.nio.charset.Charset");
-            cp.importPackage("java.nio.charset.StandardCharsets");
-            cp.importPackage("java.text.SimpleDateFormat");
-            cp.importPackage("java.util.Date");
-            cp.importPackage("java.util.Map");
-            cp.importPackage("java.util.Properties");
-            cp.importPackage("java.util.zip.Inflater");
-            cp.importPackage("java.util.zip.InflaterInputStream");
-            cp.importPackage("org.apache.commons.codec.binary.Base64");
+            cp.importPackage("com.atlassian.extras.common");
+            cp.importPackage("com.atlassian.extras.decoder.api");
+            cp.importPackage("com.atlassian.extras.keymanager");
+            cp.importPackage("java.io");
+            cp.importPackage("java.nio.charset");
+            cp.importPackage("java.text");
+            cp.importPackage("java.util");
+            cp.importPackage("org.apache.commons.codec.binary");
 
             CtClass target = cp.getCtClass(LICENSE_DECODER_CLASS);
             CtMethod verifyLicenseHash = target.getDeclaredMethod("verifyLicenseHash");
-            verifyLicenseHash.setBody("{System.out.println(\"atlassian-agent: skip hash check\");}");
+            verifyLicenseHash.setBody("{System.out.println(\"===================atlassian-agent: skip hash check===========================\");}");
+
+            target.writeFile(new File("").getAbsolutePath());
 
             return target.toBytecode();
         } catch (Exception e) {
